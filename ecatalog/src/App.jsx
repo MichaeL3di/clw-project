9905f49aa5fe87f70e9def929d3a25926ed97408
MichaeL3di <michael.triadi03@gmail.com>
2024-07-31 12:58:22 +1000

    first commit

---
+++ ecatalog/src/App.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+import Topbar from "./scenes/global/Topbar"
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value = {colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className = "content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+
+
+}
+
+export default App
